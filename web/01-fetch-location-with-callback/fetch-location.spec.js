40c34de027ff926a8fc7f7e7b5f77de50a2e1c26
WIL PANNELL <wgpannel@us.ibm.com>
2017-01-09 10:34:04 -0800

    purge non-deterministic test

--- web/01-fetch-location-with-callback/fetch-location.spec.js
+++ web/01-fetch-location-with-callback/fetch-location.spec.js
@@ -1,20 +1,5 @@
-import {fetchLocation} from './fetch-location';
-
 describe('the fetch-location canary test', () => {
   it('shows the infrastructure works', () => {
     true.should.equal(true);
-  });
-
-  describe('fetch location should', () => {
-    it('retrieve the latitude and longitude', function(done) {
-      const onSuccess = location => {
-        window.console.log(location);
-        location.should.have.property('lat');
-        location.should.have.property('long');
-        done();
-      };
-      this.timeout(10000);
-      fetchLocation(onSuccess);
-    });
   });
 });
