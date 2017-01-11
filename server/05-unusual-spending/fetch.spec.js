108953472f91edd386945bac07fcc232602946a0
WIL PANNELL <wgpannel@us.ibm.com>
2017-01-11 10:55:48 -0800

    improve code legibility

--- server/05-unusual-spending/fetch.spec.js
+++ server/05-unusual-spending/fetch.spec.js
@@ -1,34 +1,23 @@
-import td from 'testdouble';
+/*eslint dot-notation: "off"*/
+import {replace, when} from '../../test-helper';
 
 describe('fetch should', () => {
-  it('interact with months', () => {
-    const months = td.replace('./months');
-    const fetch = require('./fetch').fetch;
-
-    td.when(months.current());
-    td.when(months.prior());
-
-    fetch();
-
-    td.verify(months.current());
-    td.verify(months.prior());
-  });
-
   it('call the API with this and last month', () => {
-    const months = td.replace('./months');
-    const api = td.replace('./api').api;
+    const monthsDouble = replace('./months');
+    const apiDouble = replace('./api')['api'];
+
     const fetch = require('./fetch').fetch;
-    const userId = 'fake-user-id';
 
-    td.when(months.current()).thenReturn({year: 2016, month: 12});
-    td.when(months.prior()).thenReturn({year: 2016, month: 11});
+    when(monthsDouble.current()).thenReturn({year: 2016, month: 12});
+    when(monthsDouble.prior()).thenReturn({year: 2016, month: 11});
 
-    td.when(api(userId, {year: 2016, month: 12})).thenResolve('dummy-current-months-payments');
-    td.when(api(userId, {year: 2016, month: 11})).thenResolve('dummy-prior-months-payments');
+    when(apiDouble('dummy-user-id', {year: 2016, month: 12})).thenResolve(
+        'dummy-current-months-payments');
 
-    fetch(userId);
+    when(apiDouble('dummy-user-id', {year: 2016, month: 11})).thenResolve(
+        'dummy-prior-months-payments');
 
-    return fetch(userId).should.eventually.deepEqual({
+    return fetch('dummy-user-id').should.eventually.deepEqual({
       'current-month': 'dummy-current-months-payments',
       'prior-month': 'dummy-prior-months-payments'
     });
