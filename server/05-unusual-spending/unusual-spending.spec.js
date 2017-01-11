9d1e6a41ef5810edce6c9ad653be85469b853a6a
WIL PANNELL <wgpannel@us.ibm.com>
2017-01-11 10:55:09 -0800

    refactor to reveal intentions of test doubles

--- server/05-unusual-spending/unusual-spending.spec.js
+++ server/05-unusual-spending/unusual-spending.spec.js
@@ -3,19 +3,19 @@ import {replace, when, verify} from '../../test-helper';
 
 describe('unusual spending should', () => {
   it('interact with fetch, high-spending and email', () => {
-    const fetch = replace('./fetch')['fetch'];
-    const categorize = replace('./categorize')['categorize'];
-    const email = replace('./email')['email'];
+    const fetchDouble = replace('./fetch')['fetch'];
+    const categorizeDouble = replace('./categorize')['categorize'];
+    const emailDouble = replace('./email')['email'];
 
     let unusualSpending;
 
-    when(fetch('user-id')).thenResolve('payments');
-    when(categorize('payments')).thenReturn('categorized-payments');
+    when(fetchDouble('user-id')).thenResolve('payments');
+    when(categorizeDouble('payments')).thenReturn('categorized-payments');
 
     unusualSpending = require('./unusual-spending').unusualSpending;
 
     const receiveCategorizedPayments = () => {
-      verify(email('user-id', 'categorized-payments'));
+      verify(emailDouble('user-id', 'categorized-payments'));
     };
 
     return unusualSpending('user-id').then(receiveCategorizedPayments);
