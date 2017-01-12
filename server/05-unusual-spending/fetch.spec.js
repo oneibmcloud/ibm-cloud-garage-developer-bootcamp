381440c09fa90482cb511e08e3cf6022fd18dda7
WIL PANNELL <wgpannel@us.ibm.com>
2017-01-11 22:41:43 -0800

    simplify data format

--- server/05-unusual-spending/fetch.spec.js
+++ server/05-unusual-spending/fetch.spec.js
@@ -1,25 +1,29 @@
 /*eslint dot-notation: "off"*/
 import {replace, when} from '../../test-helper';
 
-describe('fetch should', () => {
-  it('call the API with this and last month', () => {
+describe('fetch', () => {
+  it('canary test shows the infrastructure works', () => {
+    true.should.equal(true);
+  });
+
+   it('calls the API with current and prior months', () => {
+    const currentMonth = {year: 2016, month: 12};
+    const priorMonth = {year: 2016, month: 11};
+
+    const currentMonthPayments = {month: currentMonth, payments: ['current']};
+    const priorMonthPayments = {month: priorMonth, payments: ['prior']};
+
     const monthsDouble = replace('./months');
     const apiDouble = replace('./api')['api'];
-
     const fetch = require('./fetch').fetch;
 
-    when(monthsDouble.current()).thenReturn({year: 2016, month: 12});
-    when(monthsDouble.prior()).thenReturn({year: 2016, month: 11});
+    when(monthsDouble.current()).thenReturn(currentMonth);
+    when(monthsDouble.prior()).thenReturn(priorMonth);
 
-    when(apiDouble('dummy-user-id', {year: 2016, month: 12})).thenResolve(
-        'dummy-current-months-payments');
+    when(apiDouble('user-id', currentMonth)).thenResolve(currentMonthPayments);
+    when(apiDouble('user-id', priorMonth)).thenResolve(priorMonthPayments);
 
-    when(apiDouble('dummy-user-id', {year: 2016, month: 11})).thenResolve(
-        'dummy-prior-months-payments');
-
-    return fetch('dummy-user-id').should.eventually.deepEqual({
-      'current-month': 'dummy-current-months-payments',
-      'prior-month': 'dummy-prior-months-payments'
-    });
+    return fetch('user-id').should.eventually.deepEqual(
+        [currentMonthPayments, priorMonthPayments]);
   });
 });
