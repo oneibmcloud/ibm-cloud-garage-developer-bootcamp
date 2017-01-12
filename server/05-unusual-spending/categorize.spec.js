ec6ce7c1c032163fcc4c0f44a1667d4901af6703
WIL PANNELL <wgpannel@us.ibm.com>
2017-01-11 22:44:01 -0800

    stub categorize spec

---
+++ server/05-unusual-spending/categorize.spec.js
@@ -0,0 +1,7 @@
+describe('categorize', () => {
+  it('canary test shows the infrastructure works', () => {
+    true.should.equal(true);
+  });
+
+  it('groups the payments by month and category');
+});
