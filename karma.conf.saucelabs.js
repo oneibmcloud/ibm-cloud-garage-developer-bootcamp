c3779e7bedef23784ef27d3c3e874bf2daf1162c
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-08 15:34:03 +0800

    took out phantom custom launcher

--- karma.conf.saucelabs.js
+++ karma.conf.saucelabs.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
     autoWatch: false,
     autoWatchBatchDelay: 100,
     basePath: '',
-    browsers: ['PhantomJS', 'PhantomJS_custom'],
+    browsers: ['PhantomJS'],
     colors: true,
     exclude: [],
     frameworks: ['mocha', 'should'],
