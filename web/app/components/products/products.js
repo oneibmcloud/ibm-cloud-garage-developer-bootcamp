cc803eb8124578bd9d149a7c7701ad791c2cca61
Matt Oberlin <moberlin8@gmail.com>
2017-03-09 17:01:20 +0800

    Green bar on #/products URL.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

---
+++ web/app/components/products/products.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+import {productsDirective} from './products.directive.js';
+
+import ngAnimate from 'angular-animate';
+import ngAria from 'angular-aria';
+import ngMaterial from 'angular-material';
+
+export const products = angular.module('products', [
+  uiRouter,
+  ngAnimate,
+  ngAria,
+  ngMaterial
+])
+
+.config( ($stateProvider) => {
+  $stateProvider.
+  state('products', {
+    url: '/products',
+    template: '<products></products>'
+  });
+})
+.directive('products', productsDirective);
