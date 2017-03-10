2960c4691218909978e1d5413d5d7d32caaf8e0c
Matt Oberlin <moberlin8@gmail.com>
2017-03-10 12:12:42 +0800

    Green on one test in product controller.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

---
+++ web/app/components/products/products.controller.js
@@ -0,0 +1,20 @@
+function ProductsController() {
+  let vm = this;
+  vm.title = 'Products';
+  vm.products = [
+    {
+      title: 'RF-97 Autograph',
+      variants: [{ price: 250 }],
+      image: { src: 'http://www.ibm.com/image01' }
+    }
+  ];
+  vm.message = '';
+
+  vm.getProducts = function () {
+    return vm.products;
+  };
+};
+
+ProductsController.$inject = ['productsService'];
+
+export {ProductsController};
