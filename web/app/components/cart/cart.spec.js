57b77fa7a99f7c042f3960e7486d4ab7cafa103f
Victor Leung <vicleung@au1.ibm.com>
2017-03-10 12:19:05 +0800

    added cart with dummy product

--- web/app/components/cart/cart.spec.js
+++ web/app/components/cart/cart.spec.js
@@ -1,4 +1,6 @@
 import 'script!jquery/dist/jquery';
+import angular from 'angular';
+
 import {cart} from './cart.js';
 
 describe('the cart page', () => {
@@ -6,16 +8,49 @@ describe('the cart page', () => {
   let $location;
   let $state;
 
+  let element;
+  let $ = window.$;
+
+  let buildTemplate = () => {
+    return angular.element('<cart></cart>');
+  };
+
   beforeEach(window.module('ui.router'));
   beforeEach(window.module(cart.name));
 
-  beforeEach(window.inject(($rootScope, _$state_, _$location_) => {
+  beforeEach(window.inject((
+  $rootScope, $compile, $httpBackend, _$state_, _$location_) => {
     $scope = $rootScope.$new();
     $state = _$state_;
     $location = _$location_;
 
+    element = $compile(buildTemplate())($scope);
     $scope.$digest();
   }));
+
+  it('a title', () => {
+    ($(element).find('h1').text()).should.equal('Cart Details');
+  });
+
+  it('an thumbnail image', () => {
+    ($(element).find('img[rel=thumbnail]').length).should.equal(1);
+  });
+
+  it('a product name', () => {
+    ($(element).find('span[rel=product-name]').length).should.equal(1);
+  });
+
+  it('a retail price', () => {
+    ($(element).find('span[rel=retail-price]').length).should.equal(1);
+  });
+
+  it('a discount price', () => {
+    ($(element).find('span[rel=discounted-price]').length).should.equal(1);
+  });
+
+  it('a saving', () => {
+    ($(element).find('span[rel=saving]').length).should.equal(1);
+  });
 
   it('/#cart in the url', () => {
     $location.path('/cart');
