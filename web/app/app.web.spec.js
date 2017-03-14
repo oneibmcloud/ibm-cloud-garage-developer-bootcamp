0d73a693fa7685078ab8e8efef09af4962080183
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-14 16:57:14 -0700

    see test fail with message: expected 0 not to be 0

--- web/app/app.web.spec.js
+++ web/app/app.web.spec.js
@@ -1,5 +1,26 @@
-describe('canary', () => {
-  it('proves the tests work', () => {
-    true.should.be.true();
+import 'script!jquery/dist/jquery';
+import angular from 'angular';
+
+import {entryPoint} from './app';
+
+describe('the app\'s entry point', () => {
+  let $scope;
+  let element;
+  let $ = window.$;
+
+  let buildTemplate = () => {
+    return angular.element('<app></app>');
+  };
+
+  beforeEach(window.module(entryPoint.name));
+
+  beforeEach(window.inject(($rootScope, $compile) => {
+    $scope = $rootScope.$new();
+    element = $compile(buildTemplate())($scope);
+    $scope.$digest();
+  }));
+
+  it('swaps regions in and out of the main content area', () => {
+    $(element).find('md-content[ui-view=""]').length.should.not.equal(0);
   });
 });
