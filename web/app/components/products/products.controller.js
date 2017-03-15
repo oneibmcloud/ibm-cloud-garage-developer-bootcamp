fafbf664ef6d77b02ca8faa9cd95858e26ee80ac
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-15 08:20:40 -0700

    make test bar green

--- web/app/components/products/products.controller.js
+++ web/app/components/products/products.controller.js
@@ -1,4 +1,4 @@
-function ProductsController(productsService) {
+function ProductsController(productsService, popupService) {
   const vm = this;
   vm.products = [];
 
@@ -6,10 +6,14 @@ function ProductsController(productsService) {
     return productsService.fetch(url)
     .then((products) => {
       vm.products = products;
+    })
+    .catch((error) => {
+      popupService.show('error requesting products: ' + error.message);
     });
   };
 }
 
+//noinspection JSValidateTypes
 ProductsController.$inject = ['productsService'];
 
 export {ProductsController};
