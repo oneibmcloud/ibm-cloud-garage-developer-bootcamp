36079680215563945a9d6b1c547be71d55cf113f
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-15 01:21:05 -0700

    watch test fail with message: Error: Cannot find module './service/products-service'

--- web/app/components/products/products.controller.server.spec.js
+++ web/app/components/products/products.controller.server.spec.js
@@ -1,9 +1,28 @@
 /*eslint dot-notation: "off"*/
+import {replace, when} from '../../../../test-helper'
+
 describe.only('the products controller', () => {
   it('has no products', () => {
     let ProductsController = require('./products.controller')['ProductsController'];
     let productsController = new ProductsController();
     productsController.products.should.deepEqual([]);
+  });
+
+  it('fetches products from the server', () => {
+    let productsService;
+    let ProductsController;
+    let productsController;
+
+    productsService = require('./service/products-service').productsService();
+    replace(productsService, 'fetch');
+    when(productsService.fetch()).thenResolve(['products']);
+
+    ProductsController = require('./products.controller')['ProductsController'];
+    productsController = new ProductsController(productsService);
+
+    return productsController.fetch().then(() => {
+      productsController.products.should.deepEqual(['products']);
+    });
   });
 });
 
