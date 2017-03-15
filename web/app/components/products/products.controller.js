3275615d0c0f639161ff387cef30dd53650bde2a
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-15 01:49:23 -0700

    see test pass and see regression fail with message: TypeError: Cannot read property 'fetch' of undefined

--- web/app/components/products/products.controller.js
+++ web/app/components/products/products.controller.js
@@ -2,6 +2,10 @@ function ProductsController(productService) {
   const vm = this;
   vm.products = [];
   vm.fetch = productService.fetch;
+
+  vm.fetch().then((products) => {
+    vm.products = products;
+  });
 }
 
 ProductsController.$inject = ['productService'];
