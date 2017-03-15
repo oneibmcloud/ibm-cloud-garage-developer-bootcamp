7c40f93da793e6e577bb76d4efe79239f4bbe361
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-14 23:51:49 -0700

    purge lint violations

--- spec.bundle.js
+++ spec.bundle.js
@@ -1,5 +1,10 @@
+/*eslint no-unused-vars: "off"*/
 import angular from 'angular';
+
+//noinspection JSUnresolvedVariable
 import mocks from 'angular-mocks';
+
+//noinspection JSUnresolvedVariable
 import ngMockHttp from 'ngMockHttp';
 
 let context = require.context('./web', true, /\.web.spec\.js/);
