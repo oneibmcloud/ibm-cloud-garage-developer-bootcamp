de88543faea092e436bb33511faee1d6c24c188e
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-09 12:30:21 -0700

    refactor happy path

--- server/09-reactive/canary.spec.js
+++ server/09-reactive/canary.spec.js
@@ -1,22 +1,28 @@
 import Rx from 'rxjs/Rx';
 
-describe.only('rxjs', () => {
+let separate = function() {
+  console.log('');
+  console.log('  __________________');
+  console.log('');
+};
 
-  it('observer.next(42)', (done) => {
+describe.only('rxjs', () => {
+  it('happy path: observer.next(42)', (done) => {
+    separate();
     let publisher = Rx.Observable.create(observer => {
       observer.next(42);
       observer.complete();
     });
 
     publisher.subscribe(x => {
-      console.log('  next resolves in success handler: ' + x);
+      console.log('    success handler called: ' + x);
       x.should.equal(42);
     },
     (error) => {
-      console.error(error);
+      console.info(error);
     },
     () => {
-      console.info('  done resolves in complete handler');
+      console.info('    complete handler called');
       done();
     });
   });
