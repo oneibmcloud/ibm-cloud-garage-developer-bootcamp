7f6c05dec5d0524d13d48d6d88fd107010cdd4ba
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-19 10:43:55 -0700

    make component references specific

--- web/app/components/foundation/header-ibm/header-ibm.js
+++ web/app/components/foundation/header-ibm/header-ibm.js
@@ -4,7 +4,7 @@ import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
 import ngMaterial from 'angular-material';
 
-import {headerIbmComponent} from './header-ibm.component.js';
+import {headerIbmComponent} from './header-ibm-component.js';
 
 export const headerIbm = angular.module('headerIbm', [
   ngAnimate,
