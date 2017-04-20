2251af707e26b65f1330e0a210a791b085335069
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-20 04:32:29 -0700

    add integration spec to make-component

--- make-component.js
+++ make-component.js
@@ -29,6 +29,7 @@ const controller = folder + '/' + controllerFileName;
 const component = folder + '/' + componentFileName;
 const angularModule = folder + '/' + moduleFileName;
 const spec = folder + '/' + filename + '.spec.js';
+const integration = folder + '/' + filename + '.integration.spec.js';
 
 const controllerName = filename.split('-').reduce((acc, segment) => {
   return acc + upperCase(segment);
@@ -99,15 +100,14 @@ import angular from 'angular';
 import {` + componentName + '} from \'./' + componentFileName + `';
 
 describe('the ` + filename + `', () => {
-  let $scope;
-
-  let element;
-  let $ = window.$;
+  const $ = window.$;
+  let element, $scope;
 
   let buildTemplate = () => {
     return angular.element('<` + filename + '></' + filename + `>');
   };
 
+  beforeEach(window.module('ui.router'));
   beforeEach(window.module(` + componentName + `.name));
 
   beforeEach(window.inject(($rootScope, $compile) => {
@@ -127,6 +127,67 @@ describe('the ` + filename + `', () => {
 });
 `);
 
+fs.writeFileSync(integration,
+`
+/*eslint no-undef: "off"*/
+
+import 'script!jquery/dist/jquery';
+import angular from 'angular';
+
+import {` + componentName + '} from \'./' + componentFileName + `';
+
+describe('the ` + filename + `', () => {
+  const $ = window.$;
+  let element, $scope, $location, $state, $timeout;
+
+  let buildTemplate = () => {
+    return angular.element('<` + filename + ' products="products"></' + filename + `>');
+  };
+
+  beforeEach(window.module('ui.router'));
+  beforeEach(window.module(` + componentName + `.name));
+  beforeEach(angular.mock.http.init);
+  afterEach(angular.mock.http.reset);
+
+  beforeEach(window.inject(($rootScope, $compile, _$state_, _$location_,
+  _$timeout_, $httpBackend) => {
+
+    $scope = $rootScope.$new();
+    $state = _$state_;
+    $location = _$location_;
+    $timeout = _$timeout_;
+    $httpBackend.whenGET(/.*/).passThrough();
+
+    element = $compile(buildTemplate())($scope);
+    $scope.$digest();
+  }));
+
+  describe('shows', () => {
+    it.skip('/#products in the url', () => {
+      $location.path('/products');
+      $scope.$apply();
+
+      ($state.current.url).should.equal('/products');
+      ($state.current.name).should.equal('products');
+    });
+
+    it.skip('a title', () => {
+      $(element).find('h1[rel=' + '"products-title"' + ']').text().should.equal('Products');
+    });
+
+    it.skip('products', (done) => {
+      $timeout(function() {
+
+        $(element).find('span[rel=product-title]').length.should.equal(27);
+        done();
+
+      }, 1000);
+    });
+  });
+});
+`
+);
+
 const componentBuilder = {
   html: html,
   styl: styl,
@@ -134,6 +195,7 @@ const componentBuilder = {
   component: component + '.js',
   module: angularModule + '.js',
   spec: spec,
+  integration: integration,
   folder: folder,
   filename: filename
 };
