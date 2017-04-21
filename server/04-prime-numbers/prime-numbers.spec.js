e8ae2f9b4fa622aec4c18955d85b899615ab4b26
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 05:09:45 -0700

    (1) - pass the tests

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -8,7 +8,7 @@ describe('the prime numbers canary spec', () => {
   const primeFactorsOf = (number) => {
     const factors = [];
     if (number > 1) {
-      if (number % 2 === 0) {
+      while (number % 2 === 0) {
         factors.push(2);
         number /= 2;
       }
