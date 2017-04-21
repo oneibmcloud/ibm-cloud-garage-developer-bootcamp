606f566219ee0afb41727f651a65444f12d3f59a
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 02:27:22 -0700

    design palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -6,5 +6,10 @@ describe('the palindrome canary spec', () => {
   describe('palindrome should be', () => {
     it('true for mom');
     it('false for dude');
+    it('true for mom mom');
+    it('false for dad mom');
+    it('true for whitespaces');
+    it('error for empty string');
+    it('error for not a string');
   });
 });
