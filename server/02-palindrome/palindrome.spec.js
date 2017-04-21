d1922b455f25ec0a7befe38a626a7ef0e4432362
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 02:40:22 -0700

    (1) - pass the next 3 tests

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -16,9 +16,20 @@ describe('the palindrome canary spec', () => {
       isPalindrome('dude').should.be.false();
     });
 
-    it('true for mom mom');
-    it('false for dad mom');
-    it('true for whitespaces');
+    it('true for mom mom', () => {
+      isPalindrome('mom mom').should.be.true();
+    });
+
+    it('false for dad mom', () => {
+      isPalindrome('dad mom').should.be.false();
+    });
+
+    it('true for whitespace', () => {
+      isPalindrome('').should.be.true();
+      isPalindrome(' ').should.be.true();
+      isPalindrome('     ').should.be.true();
+    });
+
     it('error for empty string');
     it('error for not a string');
   });
