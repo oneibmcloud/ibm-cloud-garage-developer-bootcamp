c6f8a8802cea78ebda32bd8a1c5f53137f4e7205
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 04:43:26 -0700

    design prime factors

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -4,5 +4,22 @@ describe('the prime numbers canary spec', () => {
   });
 
   describe('a prime numbers function should', () => {
+    it('return none for 1');
+
+    it('return 2 for 2');
+
+    it('return 3 for 3');
+
+    it('return 2, 2 for 4');
+
+    it('return 5 for 5');
+
+    it('return 2, 3 for 6');
+
+    it('return 7 for 7');
+
+    it('return 2, 2, 2 for 8');
+
+    it('return 3, 3 for 9');
   });
 });
