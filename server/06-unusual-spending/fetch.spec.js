2e1dcbc9afd9071461df4439d5880302845303bf
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 22:34:54 -0700

    see design for fetch interaction fail with message: Error: Cannot find module '/Users/wpannell/git/capstone-project/server/06-unusual-spending/months'

---
+++ server/06-unusual-spending/fetch.spec.js
@@ -0,0 +1,37 @@
+/*eslint dot-notation: "off"*/
+import {replace, when, verify, reset} from '../../test-helper';
+
+describe('fetch', () => {
+  it('should call months twice and api twice', () => {
+    const months = replace('./months');
+    const api = replace('./apiWrapper')['api'];
+
+    const userId = {dummy: 'user id'};
+    const priorMonth = {dummy: 'prior month'};
+    const currentMonth = {dummy: 'current month'};
+    const priorPayments = {dummy: 'prior month\'s payments'};
+    const currentPayments = {dummy: 'current month\'s payments'};
+
+    when(months.prior()).thenReturn(priorMonth);
+    when(months.current()).thenReturn(currentMonth);
+    when(api(userId, priorMonth)).thenReturn(priorPayments);
+    when(api(userId, currentMonth)).thenReturn(currentPayments);
+
+    let fetch = require('./fetch')['fetch'];
+
+    fetch(userId).should.deepEqual({
+      current: {
+        month: currentMonth,
+        payments: currentPayments
+      },
+      prior: {
+        month: priorMonth,
+        payments: priorPayments
+      }
+    });
+  });
+
+  afterEach(() => {
+    reset();
+  });
+});
