f21cd7972026c97277ff0a59137976a0bc27041e
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 22:49:46 -0700

    (1) pass the tests

--- server/06-unusual-spending/fetch.js
+++ server/06-unusual-spending/fetch.js
@@ -1,1 +1,20 @@
-export const fetch = () => {};
+import * as months from './months';
+import {api} from './api-wrapper';
+
+export const fetch = (id) => {
+  const current = months.current();
+  const prior = months.prior();
+  const currentPayments = api(id, current);
+  const priorPayments = api(id, prior);
+
+  return {
+    current: {
+      month: current,
+      payments: currentPayments
+    },
+    prior: {
+      month: prior,
+      payments: priorPayments
+    }
+  };
+};
