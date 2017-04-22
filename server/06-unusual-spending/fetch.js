48a686fe711c533088ff8a81c1cf3081b5686c30
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-21 23:00:35 -0700

    (3) eliminate duplication

--- server/06-unusual-spending/fetch.js
+++ server/06-unusual-spending/fetch.js
@@ -4,17 +4,15 @@ import {api} from './api-wrapper';
 export const fetch = (id) => {
   const current = months.current();
   const prior = months.prior();
-  const currentPayments = api(id, current);
-  const priorPayments = api(id, prior);
 
   return {
     current: {
       month: current,
-      payments: currentPayments
+      payments: api(id, current)
     },
     prior: {
       month: prior,
-      payments: priorPayments
+      payments: api(id, prior)
     }
   };
 };
