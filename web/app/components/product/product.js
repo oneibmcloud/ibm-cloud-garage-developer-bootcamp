09aed6d08821a22618352841093d4dcf93a8a595
omar-meshaal <omar.meshaal@gmail.com>
2017-04-24 17:14:49 +0200

    add tile as a product dependancy

--- web/app/components/product/product.js
+++ web/app/components/product/product.js
@@ -4,12 +4,14 @@ import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
 import ngMaterial from 'angular-material';
 
+import {tile} from './tile/tile';
 import {productComponent} from './product-component';
 
 export const product = angular.module('product', [
   uiRouter,
   ngAnimate,
   ngAria,
-  ngMaterial
+  ngMaterial,
+  tile.name
 ])
 .directive('product', productComponent);
