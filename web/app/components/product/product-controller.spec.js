85e4d4ebd01f2baa825c6591dbc177d10a905dbe
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-25 08:25:49 -0700

    purge dead code

--- web/app/components/product/product-controller.spec.js
+++ web/app/components/product/product-controller.spec.js
@@ -1,10 +1,3 @@
-/**
- * Created by jhtrujil on 4/21/2017.
- */
-import {replace, when, verify, reset} from '../../../../test-helper';
-
-let products;
-
 describe('product controller', () => {
   it('starts with no products', () => {
     const ProductController = require('./product-controller').ProductController;
