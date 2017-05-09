dddde8714722718f0da181cc22c684a0d3ee4ef3
WIL PANNELL <wgpannel@us.ibm.com>
2017-05-09 07:57:43 -0700

    correct false-positive (h/t @omar-meshaal)

--- server/async-03-unusual-spending/unusual-spending.js
+++ server/async-03-unusual-spending/unusual-spending.js
@@ -3,9 +3,11 @@ import {categorize} from './categorize';
 import {email} from './email';
 
 export const unusualSpending = (id) => {
-  return fetch(id).then(payments => {
-    email(id, categorize(payments));
-  }).catch(error => {
-    return new Error('fetch failed with error: ' + error.message);
+  return new Promise((resolve, reject) => {
+    fetch(id).then(payments => {
+      resolve(email(id, categorize(payments)));
+    }).catch(error => {
+      reject(new Error('fetch failed with error: ' + error.message));
+    });
   });
 };
