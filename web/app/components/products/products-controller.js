b5d91dc06ac0180482cb21b4f4204001ec4f12e4
WIL PANNELL <wgpannel@us.ibm.com>
2017-05-09 11:04:14 -0700

    correct false-positive (h/t @omar-meshaal)

--- web/app/components/products/products-controller.js
+++ web/app/components/products/products-controller.js
@@ -2,12 +2,13 @@ function ProductsController(service, popupService) {
   const vm = this;
   vm.products = [];
 
-  vm.fetch = url => {
+  vm.fetch = (url, err = () => {}) => {
     return service.fetch(url).then((response) => {
       vm.products = response.data.products;
     }).catch((error) => {
       popupService.show('Error', error.statusText + ': ' + error.status,
         'Close');
+      err(error);
     });
   };
 }
