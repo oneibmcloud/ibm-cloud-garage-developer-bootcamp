42fc81052a9c5b6fae73f5c5597b917ba2fa5aa9
WIL PANNELL <wgpannel@us.ibm.com>
2017-05-30 18:35:58 -0700

    introduce functional spec for streetName function

--- server/08-functional/007-either-example-street-name.spec.js
+++ server/08-functional/007-either-example-street-name.spec.js
@@ -1,22 +1,24 @@
 /*eslint no-shadow: "off"*/
 
+import {fromNullable} from './functional-types';
+
 describe('either example collection', () => {
   describe('street names', () => {
     const user = {address: null};
     const address = {street: null};
     const street = {name: null};
 
-    const streetName = user => {
-      const address = user.address;
-
-      if (address) {
-        const street = address.street;
-        if (street) return street.name;
-      }
-      return 'no street';
-    };
-
     describe('imperative', () => {
+      const streetName = user => {
+        const address = user.address;
+
+        if (address) {
+          const street = address.street;
+          if (street) return street.name;
+        }
+        return 'no street';
+      };
+
       it('user has no address', () => {
         user.address = null;
         streetName(user).should.equal('no street');
@@ -41,14 +43,38 @@ describe('either example collection', () => {
         user.address = address;
         streetName(user).should.equal('wil');
       });
-
     });
 
     describe('functional', () => {
-      it('user is premium', () => {
+      const streetName = user =>
+        fromNullable(user.address)
+        .chain(address => fromNullable(address.street))
+        .map(street => street.name)
+        .fold(() => 'no street', name => name);
+
+      it('user has no address', () => {
+        user.address = null;
+        streetName(user).should.equal('no street');
       });
 
-      it('user is not premium', () => {
+      it('address has no street', () => {
+        address.street = null;
+        user.address = address;
+        streetName(user).should.equal('no street');
+      });
+
+      it('street has no name', () => {
+        street.name = null;
+        address.street = street;
+        user.address = address;
+        (streetName(user) === null).should.be.true();
+      });
+
+      it('street has a name', () => {
+        street.name = 'wil';
+        address.street = street;
+        user.address = address;
+        streetName(user).should.equal('wil');
       });
     });
   });
