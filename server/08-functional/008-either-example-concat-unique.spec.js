1d3e9f529177881a6cad3b019f4518f9c4859525
WIL PANNELL <wgpannel@us.ibm.com>
2017-05-31 21:09:19 -0700

    introduce functional solution

--- server/08-functional/008-either-example-concat-unique.spec.js
+++ server/08-functional/008-either-example-concat-unique.spec.js
@@ -24,4 +24,26 @@ describe('concat unique', () => {
       concatUniq(x, ys).should.deepEqual(ys);
     });
   });
+
+  describe('functional', () => {
+    const concatUniq = (x, ys) => (
+      fromNullable(ys.filter(y => y === x)[0])
+      .fold(() => ys.concat(x), y => ys)
+    );
+
+    it('concatenates', () => {
+      const x = {};
+      const ys = [1, 2];
+      const expected = [1, 2, x];
+
+      concatUniq(x, ys).should.deepEqual(expected);
+    });
+
+    it('does not concatenates', () => {
+      const x = {};
+      const ys = [1, 2, x];
+
+      concatUniq(x, ys).should.deepEqual(ys);
+    });
+  });
 });
