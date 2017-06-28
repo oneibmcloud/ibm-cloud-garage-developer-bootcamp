0dd5cac3b90539cea37bccff0bc5041d5b13fb6b
WIL PANNELL <wgpannel@us.ibm.com>
2017-06-28 08:39:57 -0700

    adapt webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+/*eslint no-unused-vars: "off"*/
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   output: {
@@ -10,11 +12,24 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.html$/, loader: 'raw'},
+      {test: /\.styl$/, loader: 'style!css!stylus'},
+      {test: /\.css/, loader: 'style!css'},
+      {test: /\.json$/, loader: 'json-loader' },
       {
         test: /\.js$/,
         loader: 'babel',
-        exclude: [/node_modules/, /\.spec\.js/, /server/]
+        exclude: [/web\/lib/, /node_modules/, /server/]
       },
     ]
-  }
+  },
+
+  stylus: {
+    use: [require('jeet')(), require('rupture')()]
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './web/data/products.json' }
+    ])
+  ]
 };
