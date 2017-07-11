0188bb88a6d5644eb201c94c580dc9d25a1b8525
WIL PANNELL <wgpannel@us.ibm.com>
2017-07-10 23:15:38 -0700

    introduce environment-specific scripts

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 cf add-plugin-repo CF-Community http://plugins.cloudfoundry.org
 cf install-plugin blue-green-deploy -r CF-Community <<< y
-cf bgd "${CF_APP}"-dev
+cf bgd "${CF_APP}"
 
-cf create-route "${CF_SPACE}" mybluemix.net -n "${CF_APP}"-dev
-cf map-route "${CF_APP}"-dev mybluemix.net -n "${CF_APP}"-dev
+cf create-route "${CF_SPACE}" mybluemix.net -n "${CF_APP}"
+cf map-route "${CF_APP}" mybluemix.net -n "${CF_APP}"
 
 
