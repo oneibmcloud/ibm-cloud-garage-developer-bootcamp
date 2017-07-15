70020e4d08fcc398215f1e3f16e4469084ca40f7
WIL PANNELL <wgpannel@us.ibm.com>
2017-07-15 14:41:54 -0700

    purge ignore declarations from spec bundle

--- spec.bundle.js
+++ spec.bundle.js
@@ -1,11 +1,6 @@
-/*eslint no-unused-vars: "off"*/
 import td from 'testdouble';
 import angular from 'angular';
-
-//noinspection JSUnresolvedVariable
 import mocks from 'angular-mocks';
-
-//noinspection JSUnresolvedVariable
 import ngMockHttp from 'ngMockHttp';
 
 let context = require.context('./web', true, /\.spec\.js/);
