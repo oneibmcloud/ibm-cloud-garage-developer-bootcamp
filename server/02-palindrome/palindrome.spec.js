ed1c1e91b4338927f58cfd43f7c98eaa431ebfee
WIL PANNELL <wgpannel@us.ibm.com>
2017-07-28 15:45:39 -0500

    make test bar green on palindrome.spec

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,9 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function isPalindrome(word) {
-    return word === word.split('').reverse().join('');
+  function isPalindrome(phrase) {
+    if (phrase.trim().length === 0) return false;
+    return phrase === phrase.split('').reverse().join('');
   }
 
   describe('a palindrome', () => {
@@ -16,9 +17,20 @@ describe.only('the palindrome canary spec', () => {
       isPalindrome('racecars').should.be.false();
     });
 
-    it('is not race car');
-    it('"   " is not a palindrome   ');
-    it('"" is not a palindrome');
-    it('is "mom dad mom"');
+    it('is not race car', () => {
+      isPalindrome('race car').should.be.false();
+    });
+
+    it('"   " is not a palindrome   ', () => {
+      isPalindrome('   ').should.be.false();
+    });
+
+    it('"" is not a palindrome', () => {
+      isPalindrome('').should.be.false();
+    });
+
+    it('is "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
   });
 });
