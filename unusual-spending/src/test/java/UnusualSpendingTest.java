2f61b3dc6fe1c5ea2779bf76b52ed6c96751fc59
WIL PANNELL <wgpannel@us.ibm.com>
2017-08-04 12:41:19 -0500

    purge dead code

---
+++ unusual-spending/src/test/java/UnusualSpendingTest.java
@@ -0,0 +1,30 @@
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/*
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import example.FeedsWalrus;
+import example.OpensCan;
+import example.values.CannedWalrusFood;
+import example.values.Walrus;
+import example.values.WalrusFood;
+
+
+* */
+@RunWith(MockitoJUnitRunner.class)
+public class UnusualSpendingTest {
+
+	public void trigger(long userId) {
+		// TODO: This is the entry point. Start with a test of this class
+	}
+
+}
