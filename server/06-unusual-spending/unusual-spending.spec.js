1e7bf6ed882951a6ce75860ca7496b4c144c8e5d
WIL PANNELL <wgpannel@us.ibm.com>
2017-08-17 08:17:40 -0500

    stub out unusual spending spec

--- server/06-unusual-spending/unusual-spending.spec.js
+++ server/06-unusual-spending/unusual-spending.spec.js
@@ -1,2 +1,9 @@
+// import {replace, when, verify} from '../../test-helper';
+
 describe('unusual spending', () => {
+  it('canary test should always pass', () => {
+    true.should.be.true();
+  });
+
+  it('orchestrates fetch, categorize and email');
 });
