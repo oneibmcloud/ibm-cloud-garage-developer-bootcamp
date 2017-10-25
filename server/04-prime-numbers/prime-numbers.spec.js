a6d988bdad48c72ad612cb213c417cd6fcf4404c
WIL PANNELL <wgpannel@us.ibm.com>
2017-10-25 14:33:32 +0530

    design the prime factors algorithm

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-param-reassign */
+describe.only('prime factors of', () => {
+  it('canary test shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  it('1 is none');
+  it('2 is 2');
+  it('3 is 3');
+  it('4 is 2, 2');
+  it('5 is 5');
+  it('6 is 2, 3');
+  it('7 is 7');
+  it('8 is 2, 2, 2');
+  it('9 is 3, 3');
+});
