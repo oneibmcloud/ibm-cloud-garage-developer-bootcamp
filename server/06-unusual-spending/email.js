f049bd09db0fab2a65264ac0b32dc8a86173a7df
WIL PANNELL <wgpannel@us.ibm.com>
2017-10-26 14:48:31 +0530

    introduce collaboration test for unusual spending

---
+++ server/06-unusual-spending/email.js
@@ -0,0 +1,3 @@
+const email = () => {};
+
+export {email};
--- server/06-unusual-spending/unusual-spending.spec.js
+++ server/06-unusual-spending/unusual-spending.spec.js
@@ -1,5 +1,30 @@
+import {replace, when, verify} from '../../test-helper';
+
 describe.only('unusual spending', () => {
   it('canary test shows the infrastructure works', () => {
     true.should.be.true();
   });
+
+  it('orchestrates the collaboration between fetch, categorize and email', () => {
+    // arrange
+    const fetch = replace('./fetch').fetch;
+    const categorize = replace('./categorize').categorize;
+    const email = replace('./email').email;
+
+    const userId = {};
+    const payments = {};
+    const categorized = {};
+
+    when(fetch(userId)).thenReturn(payments);
+    when(categorize(payments)).thenReturn(categorized);
+
+    let unusualSpending = require('./unusual-spending').unusualSpending;
+
+    // act
+    unusualSpending(userId);
+
+    //assert
+    verify(email(userId, categorized));
+  });
+
 });
