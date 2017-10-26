a92a84323b3d0fc28f6e52ae391c0ef5ee9fba9b
WIL PANNELL <wgpannel@us.ibm.com>
2017-10-26 14:49:24 +0530

    do not forget to take off the only

--- server/06-unusual-spending/unusual-spending.spec.js
+++ server/06-unusual-spending/unusual-spending.spec.js
@@ -1,6 +1,6 @@
 import {replace, when, verify} from '../../test-helper';
 
-describe.only('unusual spending', () => {
+describe('unusual spending', () => {
   it('canary test shows the infrastructure works', () => {
     true.should.be.true();
   });
